341647554a64c4d5c8d01471e62307598fcb110f
Scott Bezek <scottbez1@gmail.com>
2020-05-23 23:49:10 -0700

    Half duplex

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,33 @@
 #define TX_HOST    VSPI_HOST
 #define DMA_CHAN_TX    2
 
-#define PIN_NUM_MISO -1
+#define PIN_NUM_MISO 19
 #define PIN_NUM_MOSI 23
 #define PIN_NUM_CLK  18
 #define PIN_NUM_CS   5
 
-#define RX_HOST HSPI_HOST
-#define DMA_CHAN_RX    1
-#define PIN_NUM_RX_MISO -1
-#define PIN_NUM_RX_MOSI 13
-#define PIN_NUM_RX_CLK  14
-#define PIN_NUM_RX_CS   15
+#define OUT_LATCH_PIN (12)//Any pin will work
+#define IN_LATCH_PIN (27) //Any pin will work
 
 spi_device_handle_t spi_tx;
 spi_device_handle_t spi_rx;
 
+spi_transaction_t tx_transaction;
+spi_transaction_t rx_transaction;
+
+WORD_ALIGNED_ATTR uint8_t buf[60];
+WORD_ALIGNED_ATTR uint8_t recvbuf[30];
+WORD_ALIGNED_ATTR uint8_t dummybuf[30];
+
+void posttx(spi_transaction_t *trans) {
+    digitalWrite(OUT_LATCH_PIN, HIGH);
+    digitalWrite(OUT_LATCH_PIN, LOW);
+}
+
+void prerx(spi_transaction_t *trans) {
+    digitalWrite(IN_LATCH_PIN, LOW);
+    digitalWrite(IN_LATCH_PIN, HIGH);
+}
 
 void setup() {
 
@@ -39,6 +51,9 @@ void setup() {
         .quadhd_io_num = -1,
         .max_transfer_sz = 1000,
     };
+    ret=spi_bus_initialize(TX_HOST, &tx_bus_config, DMA_CHAN_TX);
+    ESP_ERROR_CHECK(ret);
+
     spi_device_interface_config_t tx_device_config = {
         .command_bits=0,
         .address_bits=0,
@@ -49,93 +64,81 @@ void setup() {
         .cs_ena_posttrans=0,
         .clock_speed_hz=8*1000*1000,
         .input_delay_ns=0,
-        .spics_io_num=PIN_NUM_CS,               //CS pin
+        .spics_io_num=PIN_NUM_CS,
         .flags=0,
         .queue_size=1,
         .pre_cb=NULL,
-        .post_cb=NULL,
+        .post_cb=&posttx,
     };
-    ret=spi_bus_initialize(TX_HOST, &tx_bus_config, DMA_CHAN_TX);
-    ESP_ERROR_CHECK(ret);
     ret=spi_bus_add_device(TX_HOST, &tx_device_config, &spi_tx);
     ESP_ERROR_CHECK(ret);
 
-
-
-    //Configuration for the RX SPI bus
-    spi_bus_config_t rx_bus_config = {
-        .mosi_io_num = PIN_NUM_RX_MOSI,
-        .miso_io_num = PIN_NUM_RX_MISO,
-        .sclk_io_num = PIN_NUM_RX_CLK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 1000,
+    spi_device_interface_config_t rx_device_config = {
+        .command_bits=0,
+        .address_bits=0,
+        .dummy_bits=0,
+        .mode=2,
+        .duty_cycle_pos=0,
+        .cs_ena_pretrans=0,
+        .cs_ena_posttrans=0,
+        .clock_speed_hz=8*1000*1000,
+        .input_delay_ns=0,
+        .spics_io_num=-1, //PIN_NUM_CS,
+        .flags=0,
+        .queue_size=1,
+        .pre_cb=&prerx,
+        .post_cb=NULL,
     };
-    spi_slave_interface_config_t rx_slave_config = {
-        .spics_io_num = PIN_NUM_RX_CS,
-        .flags = 0,
-        .queue_size = 3,
-        .mode = 0,
-        .post_setup_cb = NULL,
-        .post_trans_cb = NULL
-    };
-    // //Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
-    // gpio_set_pull_mode(PIN_NUM_RX_MOSI, GPIO_PULLUP_ONLY);
-    // gpio_set_pull_mode(PIN_NUM_RX_CLK, GPIO_PULLUP_ONLY);
-    // gpio_set_pull_mode(PIN_NUM_RX_CS, GPIO_PULLUP_ONLY);
+    ret=spi_bus_add_device(TX_HOST, &rx_device_config, &spi_rx);
+    ESP_ERROR_CHECK(ret);
 
-    //Initialize SPI slave interface
-    ret=spi_slave_initialize(RX_HOST, &rx_bus_config, &rx_slave_config, DMA_CHAN_RX);
-    assert(ret==ESP_OK);
+    memset(&tx_transaction, 0, sizeof(tx_transaction));
+    tx_transaction.length = 120*4;
+    tx_transaction.tx_buffer = &buf;
 
+    memset(&rx_transaction, 0, sizeof(rx_transaction));
+    rx_transaction.length = 120/4*8;
+    rx_transaction.tx_buffer = &dummybuf;
+    rx_transaction.rx_buffer = &recvbuf;
+
+    memset(buf, 0, sizeof(buf));
+    memset(recvbuf, 0, sizeof(recvbuf));
+    memset(dummybuf, 0, sizeof(dummybuf));
+
+  pinMode(OUT_LATCH_PIN, OUTPUT);
+  digitalWrite(IN_LATCH_PIN, LOW);
+  pinMode(IN_LATCH_PIN, OUTPUT);
+  digitalWrite(IN_LATCH_PIN, HIGH);
 }
 
-WORD_ALIGNED_ATTR uint8_t buf[100];
-WORD_ALIGNED_ATTR uint8_t recvbuf[100];
 
 char b[100];
 
+uint32_t x = 0;
+
 void loop() {
     esp_err_t ret;
-    uint32_t time = micros();
-    Serial.print("Hello world!");
-    Serial.print(time);
+    Serial.print("Send ");
+    Serial.print(x);
     Serial.println();
 
-    buf[0] = time & 0xFF;
-    buf[1] = (time & 0xFF00) >> 8;
-    buf[2] = (time & 0xFF0000) >> 16;
-    buf[3] = (time & 0xFF000000) >> 24;
-
-
-    // Configure receiver
-    memset(recvbuf, 0, 100);
-    spi_slave_transaction_t rx_transaction;
-    memset(&rx_transaction, 0, sizeof(rx_transaction));
-    rx_transaction.length = 32;
-    rx_transaction.rx_buffer = recvbuf;
-    ret=spi_slave_queue_trans(RX_HOST, &rx_transaction, portMAX_DELAY);
-    assert(ret==ESP_OK);
+    buf[59] = x & 0xFF;
+    x++;
 
     // Send data
-    spi_transaction_t t;
-    memset(&t, 0, sizeof(t));
-    t.length=32;
-    t.tx_buffer=&buf;
-    ret=spi_device_polling_transmit(spi_tx, &t);
+    ret=spi_device_polling_transmit(spi_tx, &tx_transaction);
     assert(ret==ESP_OK);
 
-    // Wait for received data
-    spi_slave_transaction_t* rx_completed_transaction;
-    ret = spi_slave_get_trans_result(RX_HOST, &rx_completed_transaction, portMAX_DELAY);
-    assert(ret == ESP_OK);
+    // Receive data
+    ret=spi_device_polling_transmit(spi_rx, &rx_transaction);
+    assert(ret==ESP_OK);
 
     uint32_t v =
       recvbuf[0] |
       recvbuf[1] << 8 |
       recvbuf[2] << 16 |
       recvbuf[3] << 24;
-    sprintf(b, "Received %u\n", v);
+    sprintf(b, "Recv %u\n", v);
     Serial.println(b);
 
     delay(100);
